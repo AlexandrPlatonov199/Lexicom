eb57a16ce5b0fee8b689a274408d68884fa9935b
Alexandr <sasa20sasha19910@yandex.ru>
2024-05-06 17:44:46 +0300

    add settings redis

--- app/pkg/settings/settings.py
+++ app/pkg/settings/settings.py
@@ -41,11 +41,50 @@ class APIServer(_Settings):
     PORT: PositiveInt = 5000
 
 
+class Redis(_Settings):
+    """Redis settings."""
+
+    HOST: str = "localhost"
+
+    PORT: PositiveInt = 6379
+
+    PASSWORD: str = ""
+
+    DSN: typing.Optional[str] = None
+
+    @root_validator(pre=True)
+    def build_dsn(cls, values: dict):
+        """Build DSN for Redis.
+
+        Args:
+            values: dict with all settings.
+
+        Notes:
+            This method is called before any other validation.
+            I use it to build DSN for Redis.
+
+        Returns:
+            dict with all settings and DSN.
+        """
+
+        # Constructing the DSN using RedisDsn
+        values["DSN"] = RedisDsn.build(
+            scheme="redis",
+            host=f"@{values.get('HOST')}",
+            port=f"{values.get('PORT')}",
+            password=f"{values.get('PASSWORD')}"
+        )
+
+        return values
+
+
 class Settings(_Settings):
     """Server settings."""
 
+    #: APIServer: API settings. Contains all settings for API.
     API: APIServer
 
+    REDIS: Redis
 
 
 @lru_cache
